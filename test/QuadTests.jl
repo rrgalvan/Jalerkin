aeb9a182a94157f79c7227b649a0290557bbfc49
Rafa Rodríguez Galván <rafael.rodriguez@uca.es>
2021-10-04 21:40:33 +0200

    Fixed Travis

--- test/QuadTests.jl
+++ test/QuadTests.jl
@@ -9,7 +9,6 @@ weights = [0.5, 0.5]
 qr = Quadrature{2}(nodes, weights)
 @test quad(qr, x->x) ≈ 0.5  # Exact for P1
 @test !(quad(qr, x->x^2) ≈ 1/3)  # Not exact for P2
-@test 1==2
 
 # Compute $\int_0^1 x^3 dx$ using Simpson's quad. rule
 nodes = [0, 0.5, 1]
